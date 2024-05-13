260ca3ffb8107957a0ee410682c8981dec42f760
stevenlandis <stevenlandis@comcast.net>
2024-05-12 22:33:44 -0400

    get args from cli

--- kilojoule_rust/src/main.rs
+++ kilojoule_rust/src/main.rs
@@ -2,8 +2,14 @@ use kilojoule_rust::{eval_ast_node, Parser, Val};
 
 fn main() {
     let parser = Parser::new();
-    let ast = parser.parse("{a: 1, b: 2}");
+    let mut args = std::env::args();
+    if args.len() != 2 {
+        println!("Please call with a single argument.");
+        std::process::exit(1);
+    }
+    let query = args.nth(1).unwrap();
+    let ast = parser.parse(query.as_str());
     println!("Ast: {:?}", ast);
-    let result = eval_ast_node(&Val::from_json_str(r#"{"a": 1, "b": 2}"#), &ast);
+    let result = eval_ast_node(&Val::new_null(), &ast);
     println!("Result = {:?}", result);
 }
