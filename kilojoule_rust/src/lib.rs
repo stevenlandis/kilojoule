a72a81ac895c0a200c1096c3689e610442959e4f
stevenlandis <stevenlandis@comcast.net>
2024-06-01 16:36:17 -0400

    remove old files

--- kilojoule_rust/src/lib.rs
+++ kilojoule_rust/src/lib.rs
@@ -1,21 +1,6 @@
-mod ast_node;
-mod evaluate;
-mod lookup_rows;
-mod parser;
 mod parser2;
-mod parser_reducers;
-mod rule_type;
-mod rules;
 mod run_repl;
-mod token;
-mod token_def;
-mod token_groups;
-mod val;
 
 // External exports
-pub use ast_node::AstNode;
-pub use evaluate::eval_ast_node;
-pub use parser::Parser;
 pub use parser2::Evaluator;
 pub use run_repl::run_repl;
-pub use val::Val;
